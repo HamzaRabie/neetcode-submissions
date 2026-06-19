bfdbfedaab3021b5302cc5ce9eda3231700c4490
HamzaRabie <hamzarabie25@gmail.com>
2026-06-19 22:11:33 +0300

    Add: permutations - submission-1

---
+++ Data Structures & Algorithms/permutations/submission-1.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    List<List<int>> res = new List<List<int>>();
+    public List<List<int>> Permute(int[] nums) {
+        List<int> curr = new List<int>();
+        DFS(nums, curr);
+        return res;
+    }
+    void DFS(int[] nums , List<int> curr)
+    {
+        if(curr.Count == nums.Length){res.Add(new List<int>(curr)); return;}
+
+        for(int j=0 ; j < nums.Length ; j++)
+        {
+            if (curr.Contains(nums[j]))continue;
+
+            curr.Add(nums[j]);
+            DFS(nums , curr);
+            curr.RemoveAt(curr.Count - 1);
+        }
+    }
+}
+
